e1b95358142aaeaf4837d7ee0ec8d1c8afed271a
arthcc <arthurdaconceicaocunha@gmail.com>
2024-06-15 22:51:29 -0300

    feat: add meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ReactNode } from "react";
-import {Providers} from "@/components/providers/providers";
+import { Providers } from "@/components/providers/providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,11 +20,40 @@ export default function RootLayout({ children }: RootLayoutProps) {
     <>
       <html lang="en" suppressHydrationWarning>
         <head />
+
+        <title>Tech Ears </title>
+        <meta name="title" content="Tech Ears " />
+        <meta
+          name="description"
+          content="Improve your listening with just a few minutes per day!"
+        />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:title" content="Tech Ears " />
+        <meta
+          property="og:description"
+          content="Improve your listening with just a few minutes per day!"
+        />
+        <meta
+          property="og:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:title" content="Tech Ears " />
+        <meta
+          property="twitter:description"
+          content="Improve your listening with just a few minutes per day!"
+        />
+        <meta
+          property="twitter:image"
+          content="https://metatags.io/images/meta-tags.png"
+        />
+
         <body className={inter.className}>
-          <Providers
-          >
-            {children}
-          </Providers>
+          <Providers>{children}</Providers>
         </body>
       </html>
     </>
