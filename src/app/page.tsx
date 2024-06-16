07ec42466e8f5c049a0ca4ff29423bd0a1b3dc85
arthcc <arthurdaconceicaocunha@gmail.com>
2024-06-15 22:21:57 -0300

    feat: add links and new ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,12 @@
 "use client";
 
 import * as React from "react";
-import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import Image from "next/image";
+import newTabIcon from "@/components/svg/new-tab-light.png";
+import MenuTheme from "@/components/menuTheme/menuTheme";
 
 export default function HomePage() {
   const { theme, setTheme } = useTheme();
@@ -18,27 +14,49 @@ export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
       <header className="px-4 lg:px-6 h-14 flex items-center bg-gray-100 dark:bg-gray-900 w-full fixed top-0 z-10">
-        <div className="container mx-auto flex items-center justify-end">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button size="icon">
-                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                <span className="sr-only">Toggle theme</span>
+        <div className="container mx-auto flex items-center justify-between w-full">
+          <div className="flex items-center">
+            <div className="text-lg font-bold text-gray-800 dark:text-gray-200">
+              TechEars
+            </div>
+          </div>
+          <div className="flex items-center space-x-4">
+            <Link
+              href="/about-me"
+              className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 flex items-center"
+            >
+              <Button
+                variant="link"
+                className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
+              >
+                About Me
               </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem onClick={() => setTheme("light")}>
-                Light
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("dark")}>
-                Dark
-              </DropdownMenuItem>
-              <DropdownMenuItem onClick={() => setTheme("system")}>
-                System
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+            </Link>
+            <Link
+              href="/how-to-play"
+              className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
+            >
+              <Button
+                variant="link"
+                className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
+              >
+                How to Play?
+              </Button>
+            </Link>
+            <Link
+              href="https://pixmeacoffee.vercel.app/techears"
+              target="_blank"
+              className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
+            >
+              <Button
+                variant="link"
+                className="text-gray-800 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400"
+              >
+                <Image src={newTabIcon} alt="new tab" width={20} height={20} />
+                Donate
+              </Button>
+            </Link>
+          </div>
         </div>
       </header>
 
@@ -51,12 +69,15 @@ export default function HomePage() {
         <h3 className="mt-4 text-2xl font-semibold tracking-tight text-blue-h1 dark:text-blue-400 mb-6">
           Improve your listening with just a few minutes per day!
         </h3>
-        <Link href="/player-pages">
-              <Button>Get Started</Button>
-            </Link>
+        <div className="flex items-center space-x-4">
+          <Link href="/player-pages" className="flex items-center space-x-4">
+            <Button>Get Started</Button>
+          </Link>
+          <div className="flex items-center space-x-4">
+            <MenuTheme />
+          </div>
+        </div>
       </main>
-      
     </div>
   );
 }
-
