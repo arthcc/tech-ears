71d3c2ffc170d6ea0704de3177d1a8b9a6b987c8
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-18 12:21:07 -0300

    implements language texts handle in home page :globe_with_meridians:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,11 @@ import { Button } from "@/components/ui/button";
 import MenuTheme from "@/components/menuTheme/menuTheme";
 import Header from "@/components/header/Header";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import { LanguageContext } from "../context/LanguageContext";
+
 export default function HomePage() {
   const { theme, setTheme } = useTheme();
+  const { language } = React.useContext(LanguageContext);
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -16,16 +19,20 @@ export default function HomePage() {
       <Header />
       <main className="flex flex-col items-center justify-center flex-grow p-6 bg-gray-100 dark:bg-gray-900 pt-24">
         <h1 className="text-4xl font-bold tracking-tight lg:text-5xl mb-6">
-          The
-          <span className="text-blue-h1 dark:text-blue-400"> newest way </span>
-          to Learn English
+          {language == "english" ? "The" : "A"}
+          <span className="text-blue-h1 dark:text-blue-400">
+            {language == "english" ? " newest way " : " nova maneira "}
+          </span>
+          {language == "english" ? " to Learn English " : " para Aprender Inglês "}
         </h1>
         <h3 className="mt-4 text-2xl font-semibold tracking-tight text-blue-h1 dark:text-blue-400 mb-6">
-          Improve your listening with just a few minutes per day!
+          {language == "english"
+            ? "Improve your listening with just a few minutes per day!"
+            : "Melhore sua audição com apenas alguns minutos por dia!"}
         </h3>
         <div className="flex items-center space-x-4">
           <Link href="/player-pages" className="flex items-center space-x-4">
-            <Button>Get Started</Button>
+            <Button>{language == "english" ? "Get Started" : "Começar"}</Button>
           </Link>
           <div className="flex items-center space-x-4">
             <MenuTheme />
