317bb8c90aca683b1b80d7faaeb9a934cc0fffc2
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-18 12:49:00 -0300

    add language provider on Provider component :boom:

--- components/providers/providers.tsx
+++ components/providers/providers.tsx
@@ -5,19 +5,23 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+import { LanguageContextProvider } from "@/src/context/LanguageContext";
+
 const queryClient = new QueryClient();
 export function Providers({ children, ...props }: ThemeProviderProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      {" "}
-      <NextThemesProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </NextThemesProvider>
-    </QueryClientProvider>
+    <LanguageContextProvider>
+      <QueryClientProvider client={queryClient}>
+        {" "}
+        <NextThemesProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </NextThemesProvider>
+      </QueryClientProvider>
+    </LanguageContextProvider>
   );
 }
