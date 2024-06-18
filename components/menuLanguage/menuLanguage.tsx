09bd1d6087acc1ec775f33bd79039423697f87b3
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-18 12:46:41 -0300

    create menu language component :robot:

---
+++ components/menuLanguage/menuLanguage.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { useContext } from "react";
+import { LanguageContext } from "@/src/context/LanguageContext";
+
+export const MenuLanguage = props => {
+  const { language, setLanguage } = useContext(LanguageContext);
+
+  return (
+    <DropdownMenu {...props}>
+      <DropdownMenuTrigger asChild>
+        <Button size="icon">
+          {
+            language == "english" ?
+              <img src="/usaFlag.svg" className="h-[1.2rem] w-[1.2rem]" />
+              :
+              <img src="/brazilFlag.svg" className="absolute h-[1.2rem] w-[1.2rem]" />
+          }
+
+
+          <span className="sr-only">Toggle language</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setLanguage("english")}>English</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setLanguage("portuguese")}>Portuguese</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
