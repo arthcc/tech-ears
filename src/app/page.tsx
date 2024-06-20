1bb4da4595cf05b0a9c54d99fac9607c04c60f12
arthcc <arthurdaconceicaocunha@gmail.com>
2024-06-19 23:00:26 -0300

    style: changing page color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,33 +6,43 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import MenuTheme from "@/components/menuTheme/menuTheme";
 import Header from "@/components/header/Header";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
+
 export default function HomePage() {
   const { theme, setTheme } = useTheme();
 
   return (
-    
     <div className="flex flex-col min-h-screen">
-       <GoogleAnalytics gaId="G-R5SCDC4C8D" />
+      <GoogleAnalytics gaId="G-R5SCDC4C8D" />
       <Header />
       <main className="flex flex-col items-center justify-center flex-grow p-6 bg-gray-100 dark:bg-gray-900 pt-24">
-        <h1 className="text-4xl font-bold tracking-tight lg:text-5xl mb-6">
+        <div className="hero-background w-full h-full absolute top-0 left-0 z-0"></div>
+        <h1 className="text-4xl font-bold tracking-tight lg:text-5xl mb-6 relative z-10">
           The
-          <span className="text-blue-h1 dark:text-blue-400"> newest way </span>
+          <span className="text-mirage-500 dark:text-blue-400">
+            {" "}
+            newest way{" "}
+          </span>
           to Learn English
         </h1>
-        <h3 className="mt-4 text-2xl font-semibold tracking-tight text-blue-h1 dark:text-blue-400 mb-6">
-          Improve your listening with just a few minutes per day!
+        <h3 className="mt-4 text-2xl font-semibold tracking-tight text-mirage-600 dark:text-blue-400 mb-6 relative z-10">
+          <span> Improve your listening with just a few minutes per day!</span>
         </h3>
-        <div className="flex items-center space-x-4">
-          <Link href="/player-pages" className="flex items-center space-x-4">
-            <Button>Get Started</Button>
+        <div className="flex items-center space-x-4 relative z-10">
+          <Link href="/player-pages">
+            <Button className="btn-primary">Get Started</Button>
           </Link>
-          <div className="flex items-center space-x-4">
-            <MenuTheme />
-          </div>
+          <MenuTheme />
         </div>
       </main>
+      <footer className="bg-gray-700 dark:bg-gray-900 py-6 text-white">
+        <div className="container mx-auto text-center">
+          <p>&copy; 2024 Tech Ears.</p>
+          <div className="flex justify-center space-x-4 mt-4">
+          </div>
+        </div>
+      </footer>
+      
     </div>
   );
 }
