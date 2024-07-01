f1dc547d950dfbc433786ddb1a2ccf5826bd55b6
arthcc <arthurdaconceicaocunha@gmail.com>
2024-07-01 19:16:15 -0300

    fix: fixing btn props

--- src/components/mainButton/button.tsx
+++ src/components/mainButton/button.tsx
@@ -5,7 +5,7 @@ interface Props {
 
 export function MainButton(props: Props){
   return (
-    <button onClick={() => console.log("jisaijsa")} className="justify-center w-full flex rounded-2xl bg-button-main text-white py-3 font-semibold text-xl gap-2 hover:scale-110 transition-all">
+    <button  className="justify-center w-full flex rounded-2xl bg-button-main text-white py-3 font-semibold text-xl gap-2 hover:scale-110 transition-all">
       {props.icon}
       {props.text}
     </button>
