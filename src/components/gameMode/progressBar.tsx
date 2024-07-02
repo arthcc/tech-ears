823f932aec6a7a73b5934b523ee411a36f8342f4
arthcc <arthurdaconceicaocunha@gmail.com>
2024-07-02 11:00:33 -0300

    feat: add progress bar component

---
+++ src/components/gameMode/progressBar.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { cn } from "@/lib/utils";
+
+interface CustomProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorColor: string;
+}
+export default function ProgressBar() {
+  return 
+}
