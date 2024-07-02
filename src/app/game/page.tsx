2fb8af9270d17780d7aefeff7d4c3eb08eb10017
arthcc <arthurdaconceicaocunha@gmail.com>
2024-07-02 10:59:43 -0300

    fix: changing page component

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -3,13 +3,18 @@ import { ApiGoogle } from "../../components/apiGoogle/apiGoogle";
 import MenuTheme from "../../components/menuTheme/menuTheme";
 import Footer from "@/components/footer/footer";
 import Loading from "@/components/loadingGame/loading";
+import Mode from "@/components/gameMode/footerButton";
+import FooterButton from "@/components/gameMode/footerButton";
+import TextLogo from "@/components/gameMode/textLogo";
 
 
 const PlayerPage = () => {
   return (
     <>
       <GoogleAnalytics gaId="G-R5SCDC4C8D" />
-      <Loading />
+      <TextLogo/>
+      <FooterButton/>
+    
     </>
   );
 };
